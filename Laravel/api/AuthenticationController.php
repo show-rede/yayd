787351c495f4986da695674b9fc3a9a57ef5b6c8
show-rede <show@red-e.net>
2024-06-15 20:01:39 -0400

    test

---
+++ Laravel/api/AuthenticationController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+  
+  
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $formData = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+  
+        $formData['password'] = bcrypt($request->password);
+  
+        $user = User::create($formData);        
+  
+        return response()->json([ 
+            'user' => $user, 
+            'token' => $user->createToken('passportToken')->accessToken
+        ], 200);
+          
+    }
+  
+    public function login(Request $request)
+    {
+        $credentials = [
+            'email'    => $request->email,
+            'password' => $request->password
+        ];
+  
+        if (Auth::attempt($credentials)) 
+        {
+            $token = Auth::user()->createToken('passportToken')->accessToken;
+             
+            return response()->json([
+                'user' => Auth::user(), 
+                'token' => $token
+            ], 200);
+        }
+  
+        return response()->json([
+            'error' => 'Unauthorised'
+        ], 401);
+  
+    }
+}
